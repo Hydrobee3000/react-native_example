d95b3689ad75e3a5baf6f8b893346c9e4777b498
Алексей Заикин <hydrombee@gmail.com>
2022-08-24 14:13:09 +0200

    counter store success

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
   const [count, setCount] = useState(0)
 
   const [value, setValue] = useState('value')
-  const { getItem, setItem } = useAsyncStorage('@storage_key')
+  const { getItem, setItem, removeItem } = useAsyncStorage('@storage_key')
 
   useEffect(() => {
     readItemFromStorage()
@@ -17,12 +17,14 @@ export default function App() {
 
   const readItemFromStorage = async () => {
     const item = await getItem()
-    setValue(item)
+    // setValue(item)
+    setCount(item)
   }
 
   const writeItemToStorage = async (newValue) => {
     await setItem(newValue)
-    setValue(newValue)
+    // setValue(newValue)
+    setCount(newValue)
   }
 
   return (
@@ -30,36 +32,29 @@ export default function App() {
       <View>
         <Button
           onPress={() => {
-            setCount(count + 1)
+            setCount(parseInt(count) + 1)
           }}
           title='Click'
         />
 
-        <Text style={{ marginTop: 15 }}>You clicked {count} times</Text>
+        <Text style={{ marginTop: 15, marginBottom: 15 }}>You clicked {count} times</Text>
 
         <Button
           onPress={() => {
             setCount(0)
+            removeItem()
           }}
           title='Reset'
         />
         <Text> </Text>
         <Button
           onPress={() => {
-            storeData('first save')
+            writeItemToStorage(count.toString())
           }}
           title='Save'
         />
       </View>
-      <Text style={{ marginTop: 40, marginBottom: 15 }}>Current value: {value}</Text>
-
-      <Button
-        onPress={() => {
-          writeItemToStorage(Math.random().toString(36).substr(2, 5))
-        }}
-        title='Update value'
-      />
-      <ShowStoredValue />
+      {/* <Text style={{ marginTop: 40, marginBottom: 15 }}>Current value: {count}</Text> */}
       <StatusBar style='auto' />
     </View>
   )
@@ -74,97 +69,16 @@ const styles = StyleSheet.create({
   },
 })
 
-// export default function App() {
-//   const [count, setCount] = useState(0)
-//   const [storedValue, setStoredValue] = useState(null)
+// const sendRequest = async () => {
+//   const rawResponse = await fetch('https://httpbin.org/post', {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ a: 1, b: 'Textual content' }),
+//   })
+//   const content = await rawResponse.json()
 
-//   // const sendRequest = async () => {
-//   //   const rawResponse = await fetch('https://httpbin.org/post', {
-//   //     method: 'POST',
-//   //     headers: {
-//   //       Accept: 'application/json',
-//   //       'Content-Type': 'application/json',
-//   //     },
-//   //     body: JSON.stringify({ a: 1, b: 'Textual content' }),
-//   //   })
-//   //   const content = await rawResponse.json()
-
-//   //   console.log(content)
-//   // }
-
-//   const storeData = async (value) => {
-//     try {
-//       // const jsonValue = JSON.stringify(value)
-//       await AsyncStorage.setItem('@storage_Keys', value)
-//     } catch (e) {
-//       // saving error
-//     }
-//   }
-
-//   const getData = async () => {
-//     try {
-//       const value = await AsyncStorage.getItem('@storage_Keys')
-//       if (value !== null) {
-//         setStoredValue(value)
-//         // value previously stored
-//       }
-//     } catch (e) {
-//       // error reading value
-//     }
-//   }
-//   // AsyncStorage.clear()
-//   getData()
-//   const getAllKeys = async () => {
-//     let keys = []
-//     try {
-//       keys = await AsyncStorage.getAllKeys()
-//     } catch (e) {
-//       // read key error
-//     }
-
-//     console.log(keys)
-//     // example console.log result:
-//     // ['@MyApp_user', '@MyApp_key']
-//   }
-//   getAllKeys()
-
-//   return (
-//     <View style={styles.container}>
-//       <View>
-//         <Button
-//           onPress={() => {
-//             setCount(count + 1)
-//           }}
-//           title='Click'
-//         />
-
-//         <Text>You clicked {count} times</Text>
-
-//         <Button
-//           onPress={() => {
-//             setCount(0)
-//           }}
-//           title='Reset'
-//         />
-//         <Text> </Text>
-//         <Button
-//           onPress={() => {
-//             storeData('first save')
-//           }}
-//           title='Save'
-//         />
-//       </View>
-//       <ShowStoredValue storedValue={storedValue} />
-//       <StatusBar style='auto' />
-//     </View>
-//   )
+//   console.log(content)
 // }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// })
